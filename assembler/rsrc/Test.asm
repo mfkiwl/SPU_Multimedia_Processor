f06fc58c2556ef5ad78fa59f7ee46bf667f0e4f6
Wilmer Suarez <wilmer.suarez1230@gmail.com>
2019-03-03 18:10:04 -0500

    gitignore fix

---
+++ assembler/rsrc/Test.asm
@@ -0,0 +1,69 @@
+# Test all Instructions
+LQA 127, 0xFFFF
+lqd 127, 0x3FF(127)
+stqd 127, 0x3FF(127)
+stqa 127, 0xFFFF
+rib 0x3FFFF
+ilhu 127, 0xFFFF
+il 127, 0xFFFF
+ila 127, 0x3FFFF
+iohl 127, 0xFFFF
+a 127, 126, 125
+ai 127, 126, 0x3FF
+sf 127, 126, 125
+sfi 127, 126, 0x3FF
+clz 127, 126
+and 127, 126, 125
+andc 127, 126, 125
+andi 127, 126, 0x3FF
+or 127, 126, 125
+orc 127, 126, 125
+ori 127, 126, 0x3FF
+orx 127, 126
+xor 127, 126, 125
+NAND 127, 126, 125
+nor 127, 126, 125               
+ceq 127, 126, 125
+ceqi 127, 126, 0x3FF
+cgt 127, 126, 125
+cgti 127, 126, 0x3FF
+clgt 127, 126, 125
+clgti 127, 126, 0x3FF         
+mpy 127, 126, 125
+mpyu 127, 126, 125
+mpyi 127, 126, 0x3FF
+mpyui 127, 126, 0x3FF
+mpya 127, 126, 125, 127
+mpyh 127, 126, 125
+mpyhhu 127, 126, 125
+cntb 127, 126
+avgb 127, 126, 125
+absdb 127, 126, 125
+sumb 127, 126, 125
+shlqbii 127, 126, 125
+shlqbyi 127, 126, 125
+rotqbyi 127, 126, 125
+rotqbii 127, 126, 125
+shli 127, 126, 125
+rothi 127, 126, 125
+roti 127, 126, 125
+br 0xFFFF
+bra 0xFFFF
+bi 127
+brnz 127, 0xFFFF
+brz 127, 0xFFFF
+biz 127, 126
+binz 127, 126
+fa 127, 126, 125
+fs 127, 126, 125
+fm 127, 126, 125
+fma 127, 126, 125, 127
+fnms 127, 126, 125, 127
+fms 127, 126, 125, 127
+fceq 127, 126, 125
+fcmeq 127, 126, 125
+fcgt 127, 126, 125
+fcmgt 127, 126, 125
+stop
+lnop
+NOP
